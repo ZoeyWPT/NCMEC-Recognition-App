7f84a6be3891f26f30f04567486b57ada621a8ae
ZoeyWPT <wollastonparktechnologies@gmail.com>
2018-10-13 11:52:12 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NCMEC Recognition App
+//
+//  Created by Grace Hall on 10/13/18.
+//  Copyright © 2018 Grace Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
